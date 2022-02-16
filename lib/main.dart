690dc9dc61cac8fa9ab817db03b47f8cb2080cc0
smellycloud <52908667+smellycloud@users.noreply.github.com>
2022-02-16 23:31:23 +0530

    add firebase support

--- lib/main.dart
+++ lib/main.dart
@@ -4,47 +4,26 @@ import 'screens/authentication/authentication_screen.dart';
 import 'screens/authentication/otp_screen.dart';
 
 import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+// import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 import 'constants.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const Relove());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Relove extends StatelessWidget {
+  const Relove({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    // return MaterialApp(
-    //   title: 'Relove',
-    //   theme: ThemeData(
-    //     pageTransitionsTheme: const PageTransitionsTheme(
-    //       builders: {
-    //         TargetPlatform.android: CupertinoPageTransitionsBuilder(),
-    //       },
-    //     ),
-    //     colorScheme: const ColorScheme.light(
-    //       primary: kLightTextColor,
-    //       secondary: kPrimaryColor,
-    //     ),
-    //     primaryColor: kPrimaryColor,
-    //     splashColor: kPrimaryColor,
-    //     secondaryHeaderColor: kLightTextColor,
-    //     primaryColorLight: kPrimaryColor,
-    //     primaryColorDark: kPrimaryColor,
-    //     scaffoldBackgroundColor: Colors.white,
-    //     focusColor: kLightTextColor,
-    //     dividerColor: kLightTextColor,
-    //     fontFamily: "Montserrat",
-    //   ),
-    //   home: const AuthenticationScreen(),
-    //   routes: {
-    //     AuthenticationScreen.id: (context) => const AuthenticationScreen(),
-    //     OTPScreen.id: (context) => const OTPScreen(),
-    //   },
-    // );
     return GetMaterialApp(
         defaultTransition: Transition.zoom,
       title: 'Relove',
